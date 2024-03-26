0428a6f10862fc5eb26ccfd547f4ad9431988ba6
marshmeowllo <6633012821@student.chula.ac.th>
2024-03-27 02:28:34 +0700

    stop using libs

--- src/libs/getRestaurant.tsx
+++
@@ -1,9 +0,0 @@
-
-export default async function getRestaurant(id:string) {
-    const response = await fetch(`http://localhost:4000/api/v1/restaurants/${id}`)
-
-    if(!response){
-        throw new Error(`Failed to fetch restaurant with id of ${id}`)
-    }
-    return await response.json()
-}
