4a9e10f94429de0c3216c5994f2e4641cc8d3a9e
marshmeowllo <6633012821@student.chula.ac.th>
2024-03-27 02:16:20 +0700

    add remove

--- src/app/myTable/page.tsx
+++ src/app/myTable/page.tsx
@@ -1,10 +1,13 @@
+'use client'
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import getReservations from '@/libs/getReservations';
-import { Suspense } from 'react';
+import { MouseEventHandler, Suspense } from 'react';
 import { LinearProgress } from '@mui/material';
 import dayjs from 'dayjs';
-import Nonsession from '@/components/Nonsession';
+import deleteReservation from '@/libs/deleteReservation';
+import { useSession } from 'next-auth/react';
+
 
 export default async function Home() {
   interface RestaurantItem {
@@ -25,6 +28,7 @@ export default async function Home() {
   }
 
   interface reserveItem {
+    _id:string,
     user: string;
     id: string;
     restaurant: RestaurantItem
@@ -38,13 +42,15 @@ export default async function Home() {
     data: reserveItem[]
   }
 
-  const session = await getServerSession(authOptions);
+  const {data:session} = useSession()
 
-  if (!session || !session.user.token) return (
-    <Nonsession />
-  )
+  if (!session || !session.user.token) return null
   const reserved = await getReservations(session.user.token);
-  // const reservedItem = reserved.data;
+  const handleDelete:MouseEventHandler<HTMLButtonElement> = async(e) => {
+    const id = e.currentTarget.id
+    console.log(id)
+    await deleteReservation(session.user.token, id)
+  }
 
   return (
     <main className='w-[70%] text-center'>
@@ -75,12 +81,14 @@ export default async function Home() {
                 </div>
 
                 <div className='flex flex-row mt-2 justify-between'>
-                  <button className='w-[141px] h-[37px] border border-stone-800 relative overflow-hidden transition-transform duration-300 ease-in-out 
+                  <button id={item._id} className='w-[141px] h-[37px] border border-stone-800 relative overflow-hidden transition-transform duration-300 ease-in-out 
                         hover:shadow-lg hover:shadow-stone-500/100 bg-stone-100 hover:bg-red-800 text-stone-800 hover:text-stone-100 transform 
-                        hover:-translate-x-1 hover:-translate-y-1'>Delete</button>
+                        hover:-translate-x-1 hover:-translate-y-1' onClick={handleDelete}>Delete</button>
                   <button className='w-[141px] h-[37px] border border-stone-800 relative overflow-hidden transition-transform duration-300 ease-in-out 
                         hover:shadow-lg hover:shadow-stone-500/100 bg-stone-100 hover:bg-stone-800 text-stone-800 hover:text-stone-100 transform 
-                        hover:-translate-x-1 hover:-translate-y-1'>Update</button>
+                        hover:-translate-x-1 hover:-translate-y-1'
+                        
+                        >Update</button>
                 </div>
               </div>
             ))
