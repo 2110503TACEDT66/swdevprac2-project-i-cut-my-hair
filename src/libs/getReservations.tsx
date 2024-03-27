96249f05acd386a812a045fe049895450330345c
Kasidis Sae-koh <85777908+marshmeowllo@users.noreply.github.com>
2024-03-27 09:30:20 +0700

    Update getReservations.tsx

--- src/libs/getReservations.tsx
+++ src/libs/getReservations.tsx
@@ -1,5 +1,5 @@
 export default async function getReservations(token: string) {
-    const response = await fetch("http://localhost:4000/api/v1/reservations", {
+    const response = await fetch("${process.env.BACKEND_URL}/api/v1/reservations", {
 
         method: "GET",
         headers: {
