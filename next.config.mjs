4505f79219f83e7e027c5d9a27ecf5b081c22c33
Kasidis Sae-koh <85777908+marshmeowllo@users.noreply.github.com>
2024-03-27 09:24:44 +0700

    Update next.config.mjs\

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
       domains: ['images.unsplash.com'],
     },
+    env: {
+        FRONTEND_URL = process.env.FRONTEND_URL,
+        BACKEND_URL = process.env.BACKEND_URL
+    }
     // other configurations...
   };
   
