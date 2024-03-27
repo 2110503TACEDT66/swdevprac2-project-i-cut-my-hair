896546cda72f744694ef4f433182113d1bf3f990
Kasidis Sae-koh <85777908+marshmeowllo@users.noreply.github.com>
2024-03-27 09:31:28 +0700

    Update getUserProfile.tsx

--- src/libs/getUserProfile.tsx
+++ src/libs/getUserProfile.tsx
@@ -1,6 +1,6 @@
 export default async function getUserProfile(token:string) {
     console.log(token)
-    const response = await fetch("http://localhost:4000/api/v1/auth/me",
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/me`,
     {
         method: "GET",
         headers:{
