b98c75d889ea6bad5a2cddd8ccebdc18adb36a0a
Kasidis Sae-koh <85777908+marshmeowllo@users.noreply.github.com>
2024-03-27 09:31:58 +0700

    Update updateReservation.tsx

--- src/libs/updateReservation.tsx
+++ src/libs/updateReservation.tsx
@@ -1,5 +1,5 @@
 export default async function updateReservation(token:string, resid:string, resvTime:string){
-    const response = await fetch(`http://localhost:4000/api/v1/reservations/${resid}`, {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/reservations/${resid}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
